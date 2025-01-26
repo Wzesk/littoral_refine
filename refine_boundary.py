7fa6579965ab6560b72dd561c5ca09a682d81c19
wzesk <walter@conformlab.com>
2025-01-26 08:08:49 -0500

    migrated periodic/nonperiodic feature into refine boundary

--- refine_boundary.py
+++ refine_boundary.py
@@ -6,14 +6,14 @@ import math
 from geomdl import fitting
 import numpy as np
 from PIL import Image
-import matplotlib.pyplot as plt
 
 
 
 class boundary_refine:
-  def __init__(self,shoreline_path,img_path,delta=0.005):
+  def __init__(self,shoreline_path,img_path,delta=0.005,periodic=True):
     self.shoreline_path = shoreline_path
     self.refined_filepath = None
+    self.periodic = periodic
 
     #load the image
     self.img = Image.open(img_path)
@@ -25,6 +25,13 @@ class boundary_refine:
 
     #load the shoreline
     self.shoreline = np.genfromtxt(shoreline_path, delimiter=',')
+
+    #if the shoreline is periodic, make sure it is closed
+    if self.periodic:
+      # Close the polyline if it isn't already
+      if not np.all(self.shoreline[0] == self.shoreline[-1]):
+          #add the first point to the end of the np array
+          self.shoreline = np.vstack((self.shoreline,self.shoreline[0]))
     
     # initialize shoreline properties
     self.area = None
@@ -35,8 +42,9 @@ class boundary_refine:
 
     # determine if this is a useful boundary after area and perimeter have been set
     self.valid = True
-    if self.area < (self.img.size[0] * self.img.size[1] / 20 ): # if the area is less than 5% of the image there must be a problem
-      self.valid = False
+    if self.periodic:
+      if self.area < (self.img.size[0] * self.img.size[1] / 20 ): # if the area is less than 5% of the image there must be a problem
+        self.valid = False
 
     if self.perimeter > ((self.img.size[0] + self.img.size[1]) * 3 ): # if the length is super long there must be a problem
       self.valid = False
@@ -81,11 +89,11 @@ class boundary_refine:
 
     return self.refined_filepath
 
-  def fit_nurbs(self,degree=3,size=24,periodic=True):
-    #if periodic, set the last point equal to the first point
+  def fit_nurbs(self,degree=3,size=24):
     shoreline = self.shoreline
 
-    if periodic:
+    #if periodic, set the last point equal to the first point
+    if self.periodic:
         shoreline[len(shoreline)-1] = shoreline[0]
 
     #convert shoreline into a list of tuples
@@ -162,15 +170,12 @@ class boundary_refine:
       if len(points) < 3:
           return None  # Need at least 3 points for a closed shape
 
-      # Close the polyline if it isn't already
-      if not np.all(points[0] == points[-1]):
-          points.append(points[0])
-
       # Calculate the area using the shoelace formula
       area = 0
-      for i in range(len(points) - 1):
-          area += (points[i][0] * points[i+1][1] - points[i+1][0] * points[i][1])
-      area = abs(area) / 2
+      if self.periodic:
+        for i in range(len(points) - 1):
+            area += (points[i][0] * points[i+1][1] - points[i+1][0] * points[i][1])
+        area = abs(area) / 2
 
       # Calculate the perimeter
       perimeter = 0
@@ -279,31 +284,6 @@ class boundary_refine:
       refined_boundary_pts.append(boundary_val)
 
     return refined_boundary_pts
-  
-  #visualize the output
-  def visualize_results(self,draw_image=False,draw_sampling=False):
-    plt.axis('equal')
-    plt.rcParams['figure.figsize'] = [25, 25]
-    plt.grid(linestyle=':', color='0.5') 
-    plt.gca().invert_yaxis()
-
-    self.sample_image
-    if draw_image:
-        img_arr = np.array(self.img)
-        plt.imshow(img_arr) # check alignment with original image
-
-    if draw_sampling:
-        sampled_nir = self.sample_values
-        for t_s in sampled_nir:
-            for pt in t_s:
-                pixel = np.array([pt[2][0]/255,pt[2][1]/255,pt[2][2]/255])
-                plt.plot(pt[0], pt[1], '.',ms=5,color=pixel)
-
-    plt.plot(self.shoreline[:,0],self.shoreline[:,1],color='blue')
-    plt.plot(self.refined_boundary[:,0],self.refined_boundary[:,1],color='red')
-
-    plt.show()
-
 
   #this function is directly from coastsat!!
   #use mask as labels
@@ -363,7 +343,7 @@ class boundary_refine:
 
   def save_refined_shoreline(self):
     #save the refined shoreline to a csv file
-    self.refined_filepath = self.shoreline_path.replace('_sl.csv','_rl.csv')
+    self.refined_filepath = self.shoreline_path.replace('_sl','_rl')
     np.savetxt(self.refined_filepath, self.refined_boundary, delimiter=",", fmt="%f")
 
 ################################################################################
